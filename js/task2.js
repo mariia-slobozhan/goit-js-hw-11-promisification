88269bdee9052c811e0d919cb16762e05812c123
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-08-15 18:18:15 +0300

    1-2 tasks

---
+++ js/task2.js
@@ -0,0 +1,27 @@
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+  
+  const logger = updatedUsers => console.table(updatedUsers);
+
+
+const toggleUserState = (allUsers, userName) =>{
+    const promise = new Promise((resolve, reject) => {
+        const updatedUsers = allUsers.map(user => 
+            user.name === userName ? { ...user, active: !user.active } : user,
+          );
+
+          resolve(updatedUsers)
+          reject(users)
+
+    })
+    return promise;
+
+}
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
